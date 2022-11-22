cb59d9a5c852016bd9146a814f059b270deaa380
Andrey Charikov <andreycharikov@Andreys-MacBook-Pro-2.local>
2022-11-22 13:49:14 +0200

    deploy 5

--- backend/foodgram/foodgram/settings.py
+++ backend/foodgram/foodgram/settings.py
@@ -73,12 +73,13 @@ WSGI_APPLICATION = 'foodgram.wsgi.application'
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
 # DATABASES = {
-#
 #    'default': {
 #        'ENGINE': 'django.db.backends.sqlite3',
 #        'NAME': BASE_DIR / 'db.sqlite3',
 #    }
 # }
+
+FILTERS_DEFAULT_LOOKUP_EXPR = 'icontains'
 
 DATABASES = {
     'default': {
